16012ef82b235be813d236103b53c07ff9e9321f
rishavchanda <rishavchanda0@gmail.com>
2024-02-03 01:37:24 +0530

    .

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://ai-image-generator-q6sy.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
